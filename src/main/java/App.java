4ee0c5e3a2220f6e704b2f1c3fd9f090b5cd9922
kkqor0702 <delico3@naver.com>
2026-07-29 16:29:07 +0900

    c13: 목록

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
     int lastId = 0;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
 
     public App(Scanner sc) {
         this.sc = sc;
@@ -18,12 +21,25 @@ public class App {
             switch (cmd) {
                 case "등록" -> {
                     System.out.print("명언 : ");
-                    String saying = sc.nextLine();
+                    String content = sc.nextLine();
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
 
                     lastId++;
+                    WiseSaying wiseSaying = new WiseSaying(lastId, content, author);
+                    wiseSayings.add(wiseSaying);
+
                     System.out.printf("%d번 명언이 등록되었습니다.%n", lastId);
+                }
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n", wiseSaying.getId(),
+                                    wiseSaying.getAuthor(), wiseSaying.getContent()));
                 }
                 case "종료" -> {
                     return;
