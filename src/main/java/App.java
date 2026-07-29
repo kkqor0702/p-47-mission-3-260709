f2bf429c8f98eaeeb6a09cfaffddbcdf21d52330
kkqor0702 <delico3@naver.com>
2026-07-29 16:33:10 +0900

    c14: controller 도입

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
-    int lastId = 0;
-    List<WiseSaying> wiseSayings = new ArrayList<>();
+    private WiseSayingController wiseSayingController;
+    private SystemController systemController = new SystemController();
 
     public App(Scanner sc) {
         this.sc = sc;
+        this.wiseSayingController = new WiseSayingController(sc);
     } // 키보드 입력 뿐 아니라 조작한 문자열 입력도 받을 수 있음
 
     public void run() {
@@ -19,29 +18,10 @@ public class App {
             String cmd = sc.nextLine();
 
             switch (cmd) {
-                case "등록" -> {
-                    System.out.print("명언 : ");
-                    String content = sc.nextLine();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine();
-
-                    lastId++;
-                    WiseSaying wiseSaying = new WiseSaying(lastId, content, author);
-                    wiseSayings.add(wiseSaying);
-
-                    System.out.printf("%d번 명언이 등록되었습니다.%n", lastId);
-                }
-                case "목록" -> {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    wiseSayings
-                            .reversed()
-                            .stream()
-                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n", wiseSaying.getId(),
-                                    wiseSaying.getAuthor(), wiseSaying.getContent()));
-                }
+                case "등록" -> wiseSayingController.actionAdd();
+                case "목록" -> wiseSayingController.actionList();
                 case "종료" -> {
+                    systemController.actionExit();
                     return;
                 }
             }
