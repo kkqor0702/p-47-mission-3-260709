b4124434ab6ab55b89d5d07b0c61accb7977607f
kkqor0702 <delico3@naver.com>
2026-07-29 15:32:24 +0900

    c6: 출력 가로채기 메서드

--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,7 +1,37 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
+    private static PrintStream ORIGINAL_OUT = System.out;
+    private static PrintStream CURRENT_OUT = System.out;
+
     public static Scanner genScanner(String input) {
-        return new Scanner(input);
+        return new Scanner(input.trim().stripIndent());
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+
+        ORIGINAL_OUT = System.out; // 원래 출력을 변수에 저장
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        // 출력 바이트 스트림 만듦
+        PrintStream printStream = new PrintStream(outputStream);
+        // 문자열 쉽게 출력하도록 바이트를 감싸는 PrintStream 생성
+        // 프로그램 -> printstream (보조스트림) -> ByteStream(outputstream)
+        System.setOut(printStream);
+        // 자바의 기본 출력 장치를 모니터가 아니라, 방금 만든 메모리 통로(printstream) 으로 나오도록
+
+        CURRENT_OUT = printStream;
+        // 나중에 닫기 위해서 현재 통로 저장
+
+        return outputStream;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream outputStream) throws IOException {
+        System.setOut(ORIGINAL_OUT);
+        // 원래로 출력 되돌리기
+        outputStream.close();
+        CURRENT_OUT.close();
     }
 }
--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,6 +27,25 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(saying).isEqualTo("너 자신을 알라");
         assertThat(author).isEqualTo("소크라테스");
+
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()")
+    void t2() throws IOException {
+
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray(); // 모니터 => 배열
+
+        System.out.println("1 / 이순신 / 나의 죽음을 적에게 알리지 마라");
+
+        String outStr = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream); // 배열 => 모니터
+
+        assertThat(outStr).isEqualTo("1 / 이순신 / 나의 죽음을 적에게 알리지 마라\n");
+
+
+        System.out.println("이제 화면에 출력됩니다.");
 
     }
 }
