2abe752282674eca64d5f7bfc84749ada4e6b955
kkqor0702 <delico3@naver.com>
2026-07-29 15:41:08 +0900

    c7: 명언 앱 출력

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    private Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    } // 키보드 입력 뿐 아니라 조작한 문자열 입력도 받을 수 있음
+
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+    }
+}
